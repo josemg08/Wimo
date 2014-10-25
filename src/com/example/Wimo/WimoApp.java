08c1a0eddd319143062bee221ee263aaad13713a
Jose M. Gonzalez <josemg08@gmail.com>
2014-10-25 10:45:58 -0300

    Initial commit.

---
+++ src/com/example/Wimo/WimoApp.java
@@ -0,0 +1,26 @@
+package com.example.Wimo;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+/**
+ * Created by JoseMaria on 24/10/14.
+ */
+
+public class WimoApp extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+
+    public void jumpToLogin(View view){
+        Intent intent = new Intent(this, login.class);
+        startActivity(intent);
+    }
+}
---
+++ src/com/example/Wimo/tutorial.java
@@ -0,0 +1,18 @@
+package com.example.Wimo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by JoseMaria on 24/10/14.
+ */
+
+public class tutorial extends Activity{
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tutorial);
+    }
+
+}
