dcce37be08bc1394490b4f696fc8568ce0ab87ae
Jose M. Gonzalez <josemg08@gmail.com>
2015-02-24 23:46:51 -0300

    Changes in login layout

--- app/src/main/java/com/wheelsinmotion/jose/wimo/activity/Activitylogin.java
+++ app/src/main/java/com/wheelsinmotion/jose/wimo/activity/Activitylogin.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import com.wheelsinmotion.jose.wimo.R;
 
